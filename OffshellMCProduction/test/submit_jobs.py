2c52cbeed4354656406e93d389dc7a3fa2de63f4
Ulascan Sarica <ulascan90@gmail.com>
2020-10-31 19:13:47 -0700

    Veto vanderbilt

--- OffshellMCProduction/test/submit_jobs.py
+++ OffshellMCProduction/test/submit_jobs.py
@@ -17,7 +17,7 @@ def run(csvs, tag, direct_submit, doTestRun):
     scram_arch = os.getenv("SCRAM_ARCH")
     cmssw_version = os.getenv("CMSSW_VERSION")
 
-    allowed_sites="T2_US_UCSD,T2_US_Caltech,T2_US_MIT,T2_US_Purdue,T2_US_Vanderbilt,T2_US_Wisconsin,T3_US_UCR,T3_US_Baylor,T3_US_Colorado,T3_US_NotreDame,T3_US_Rice,T3_US_Rutgers,T3_US_UMD,T3_US_Vanderbilt_EC2,T3_US_OSU"
+    allowed_sites="T2_US_UCSD,T2_US_Caltech,T2_US_MIT,T2_US_Purdue,T2_US_Wisconsin,T3_US_UCR,T3_US_Baylor,T3_US_Colorado,T3_US_NotreDame,T3_US_Rice,T3_US_Rutgers,T3_US_UMD,T3_US_OSU"
 
     seed = 12345
     for fname in csvs:
